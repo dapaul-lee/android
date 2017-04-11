df98f3fa83061d41cfec5f9898a22810d7045377
dapaul <dapaul.lee@qq.com>
2017-04-11 12:25:38 +0800

    execute retrofix async and sync
    execute retrofix async and sync

--- DpAndroidTest/app/src/main/java/com/dp/test/ui/activity/RetrofitActivity.java
+++ DpAndroidTest/app/src/main/java/com/dp/test/ui/activity/RetrofitActivity.java
@@ -5,21 +5,33 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.dp.test.R;
+import com.dp.test.debug.DpDebug;
+
+import java.io.IOException;
 import java.util.List;
+
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Url;
 
 public class RetrofitActivity extends AbstractActivity {
 
     private TextView mTvResult;
 
     String API_BASE_URL = "https://api.github.com/";
+
+    private static final String BTKITTY_URL = "http://btkitty.bid/";
 
     OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
@@ -31,13 +43,20 @@ public class RetrofitActivity extends AbstractActivity {
 
     Call<List<GitHubRepo>> call = client.reposForUser("fs-opensource");
 
+    private BTKittySearchApi mBTKittySearchApi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
         mTvResult = (TextView) findViewById(R.id.tv_result);
-        run();
+        btKittyInit();
+//        run();
+//        runBTKittyAsync("girl");
+//        runBTKittySync("beauty girl");
+//        runBTKittyGetAsync(BTKITTY_URL);
+        runBTKittyGetSync(BTKITTY_URL);
     }
 
     private void run() {
@@ -86,5 +105,101 @@ public class RetrofitActivity extends AbstractActivity {
         Call<List<GitHubRepo>> reposForUser(
                 @Path("user") String user
         );
+    }
+
+    public interface BTKittySearchApi {
+        @FormUrlEncoded
+        @Headers({"User-Agent:Mozilla"})
+        @POST("http://btkitty.bid/")
+        Call<ResponseBody> mainSearch(@Field("keyword") String keyword);
+
+        @GET
+        Call<ResponseBody> subSearch(@Url String url);
+    }
+
+    public void btKittyInit() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BTKITTY_URL)
+                .build();
+        mBTKittySearchApi = retrofit.create(BTKittySearchApi.class);
+    }
+
+    private void runBTKittyAsync(String keyword) {
+//        String body = "";
+        Call<ResponseBody> call = mBTKittySearchApi.mainSearch(keyword);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                String body = "";
+                try {
+                    body = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                DpDebug.log("RetrofixActivity ---- runBTKitty ---- onResponse ---- body : " + body);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                DpDebug.log("RetrofixActivity ---- runBTKitty ---- onFailure");
+            }
+        });
+    }
+
+    private void runBTKittySync(final String keyword) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String body = "";
+                Call<ResponseBody> call = mBTKittySearchApi.mainSearch(keyword);
+                try {
+                    Response<ResponseBody> bodyResponse = call.execute();
+                    body = bodyResponse.body().string();//获取返回体的字符串
+                } catch (IOException e) {
+                    DpDebug.log("RetrofixActivity ---- runBTKittySync ---- IOException : " + e.toString());
+                    e.printStackTrace();
+                }
+                DpDebug.log("RetrofixActivity ---- runBTKittySync ---- body : " + body);
+            }
+        }).start();
+    }
+
+    private void runBTKittyGetAsync(final String url) {
+        Call<ResponseBody> call = mBTKittySearchApi.subSearch(url);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                String body = "";
+                try {
+                    body = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                DpDebug.log("RetrofixActivity ---- runBTKittyGetAsync ---- onResponse ---- body : " + body);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                DpDebug.log("RetrofixActivity ---- runBTKittyGetAsync ---- onFailure");
+            }
+        });
+    }
+
+    private void runBTKittyGetSync(final String url) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String body = "";
+                Call<ResponseBody> call = mBTKittySearchApi.subSearch(url);
+                try {
+                    Response<ResponseBody> bodyResponse = call.execute();
+                    body = bodyResponse.body().string();//获取返回体的字符串
+                } catch (IOException e) {
+                    DpDebug.log("RetrofixActivity ---- runBTKittyGetSync ---- IOException : " + e.toString());
+                    e.printStackTrace();
+                }
+                DpDebug.log("RetrofixActivity ---- runBTKittyGetSync ---- body : " + body);
+            }
+        }).start();
     }
 }
